ad06ee86693a690f1960f1ad466125c068d56cf2
kevinlbnguyen113 <36432653+kevinlbnguyen113@users.noreply.github.com>
2018-10-30 17:11:40 -0700

    Add files via upload
    added answer choice with random false answer for multiple choice quiz.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,17 @@ int main(){
     cout<<"Please select which math operation to practice:\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division";
     cin>> mathChoice;
     cout<<"What type of Game Mode:\n1.Learn\n2.Practice\n3.Time Attack!\n 4.Expert Mode\n ";*/
-    divisionQuestionExpert();
-    return 0;
+    //divisionQuestionExpert();
+    additionQuestionEz();
+    randomAnswer();
+    additionQuestionEz();
+    randomAnswer();
+    additionQuestionEz();
+    randomAnswer();
+    additionQuestionEz();
+    randomAnswer();
+
+
+    
+    return 99;
 }
--- mathquestion.h
+++ mathquestion.h
@@ -2,10 +2,17 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <vector>
+#include <cstdlib>
 
 
 using namespace std;
 int answer, compAwsr,gradeLevel,mathChoice,userAnswer;
+vector<int>randQuest;
+int *answerPointer;
+int *arrayPointer;
+
+
 
 char a=toupper(97), b=toupper(98), c=toupper(99),d=toupper(100);
 bool isTrue=false;
@@ -15,6 +22,7 @@ void additionQuestionEz(){
     firstNumber= 1+(rand()%9);
     secondNumber=1+(rand()&9);
     answer  = firstNumber + secondNumber;
+    answerPointer= &answer;
     cout<< firstNumber<<" + "<<secondNumber <<" = ";
     cin>>userAnswer;
     while (userAnswer != answer){
@@ -301,3 +309,38 @@ void divisionQuestionExpert(){
     }
     
 }
+void randomAnswer(){
+
+    int questRan[4]={-1,-1,-1,-1};
+    
+    srand(time(NULL));
+    unsigned int x = (rand()%4);
+    questRan[x]=*answerPointer;
+    for (int i=0; i< 4;i++){
+        if (i!=x){
+            int y = (rand()%9)-1;
+            questRan[i]= y;
+            
+        }
+        cout<< questRan[i]<<"\t";
+    }
+    cout<<endl;
+    arrayPointer=questRan;
+}
+/*void setMultipleChoice(){
+    srand(time(NULL));
+    unsigned int x =1+ (rand()%9);
+    for(int a =0;a<4;a++){
+        if(arrayPointer[a]==*answerPointer){
+            arrayPointer[a]= *answerPointer;
+        }
+        if (arrayPointer[a]==-1){
+            unsigned int x =1+ (rand()%9);
+            arrayPointer[a]= x;
+        }
+    }
+    for (int i =0;i<4;i++){
+            cout<< arrayPointer[i]<<"\t";
+    }
+    cout<<endl;
+}*/
